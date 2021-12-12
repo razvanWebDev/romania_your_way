1cc67008dd6ee49ab1085a577bf29db978e4275b
razvanWebDev <crsn_razvan@yahoo.com>
2021-12-12 20:35:07 +0200

    fixed essentials carousel

--- PHP/db.php
+++ PHP/db.php
@@ -4,12 +4,12 @@ $secret_key = '6LcP9C8cAAAAALWcIr5gGzpBi2EcbtYfEsI5V1gD';
 
 // ONLINE (infinityfree)=============
 
-// $server = 'sql108.epizy.com';
-// $username = 'epiz_29581335';
-// $password = 'bTEi09aoTZ9P';
-// $dbname = 'epiz_29581335_new_template'; 
+// $server = 'sql308.epizy.com';
+// $username = 'epiz_30575221';
+// $password = '1NnkZTaho7';
+// $dbname = 'epiz_30575221_romania_your_way'; 
 
-$website_url = "www.website-template.great-site.net";
+$website_url = "www.romania-your-way.great-site.net";
 
 //LOCAL=================================
   $server = 'localhost';
